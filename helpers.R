43ba71a6cb063591e93a548133dc1edb6ad271e4
Sebastian Hönel <sebastian.honel@lnu.se>
2019-03-23 10:57:06 +0100

    Initial commit including all experiments, a short readme and all figures

---
+++ helpers.R
@@ -0,0 +1,158 @@
+install.packagesCond <- function(pkg) {
+  if (pkg %in% rownames(installed.packages()) == FALSE) {
+    install.packages(pkg, repos = "http://cran.us.r-project.org")
+  }
+}
+
+install.packagesCond("RMariaDB")
+library(RMariaDB)
+
+getExperimentConn = function() {
+  return(dbConnect(
+    RMariaDB::MariaDB(),
+    default.file=normalizePath("../my.cnf"),
+    group="experiments")
+  )
+}
+
+getDataset = function(dsName, removeUnwantedColums = TRUE) {
+  conn <- getExperimentConn()
+  result <- dbSendQuery(conn, paste("SELECT * FROM ", dsName))
+  ds <- dbFetch(result)
+  
+  if (removeUnwantedColums) {
+    removeNames <- c("SHA1",
+                     #"RepoPathOrUrl",
+                     "AuthorName", "CommitterName", "AuthorTime",
+                     "CommitterTime", "MinutesSincePreviousCommit", "Message",
+                     "AuthorEmail", "CommitterEmail",
+                     "AuthorNominalLabel", "CommitterNominalLabel",
+                     "ParentCommitSHA1s")
+    
+    ds <- ds[, !names(ds) %in% removeNames]
+  }
+  
+  dbClearResult(result)
+  dbDisconnect(conn)
+  return(ds)
+}
+
+
+# Taken from https://stackoverflow.com/questions/10286473/rotating-x-axis-labels-in-r-for-barplot
+rotate_x <- function(data, column_to_plot, labels_vec, rot_angle) {
+  plt <- barplot(data[[column_to_plot]], col='steelblue', xaxt="n")
+  text(plt, par("usr")[3], labels = labels_vec, srt = rot_angle, adj = c(1.1,1.1), xpd = TRUE, cex=0.6) 
+}
+
+
+# Returns a list of tuples of indexes or names of correlated attributes
+findCorrelationAttrs <- function(corrMatrix, cutoff = 0.9, names = TRUE) {
+  namesRows <- rownames(corrMatrix)
+  pairs <- list()
+  len <- nrow(corrMatrix)
+  for (i in 1:len) {
+    for (j in 1:len) {
+      if (i == j) {
+        next
+      }
+      if (corrMatrix[i,j] > cutoff) {
+        tuple <- c(i, j, corrMatrix[i,j])
+        if (names) {
+          tuple <- c(namesRows[i], namesRows[j], corrMatrix[i,j])
+        }
+        pairs[[paste(i, j, sep = "_")]] <- tuple
+      }
+    }
+  }
+  return(pairs)
+}
+
+
+predictLevin <- function(leftModel, rightModel, validationSamples) {
+  l <- list()
+  
+  for (row in 1:nrow(validationSamples)) {
+    temp <- validationSamples[row, ]
+    hasKws <- rowSums(temp[as.vector(
+      sapply(colnames(temp), function(x) grepl("^kw_", x)))])[[1]] > 0
+    
+    if (hasKws) {
+      label <- predict(leftModel, temp)[1]
+    } else {
+      label <- predict(rightModel, temp)[1]
+    }
+    
+    l[[row]] <- label
+  }
+  
+  return(unlist(l))
+}
+
+
+combineVotes <- function(votesLeft, votesRight) {
+  l <- list()
+  
+  for (i in 1:nrow(votesLeft)) {
+    temp <- list(
+      a = votesLeft[i, "a"] + votesRight[i, "a"],
+      c = votesLeft[i, "c"] + votesRight[i, "c"],
+      p = votesLeft[i, "p"] + votesRight[i, "p"]
+    )
+    temp <- temp[order(sapply(temp, max), decreasing = TRUE)]
+    
+    l[[i]] <- names(temp)[[1]]
+  }
+  return(as.factor(unlist(l)))
+}
+
+
+predictZeroR <- function(trainLabels) {
+  # First argument: column of a dataframe containing labels
+  t <- table(trainLabels)
+  m <- which.max(t)
+  return(rep(as.factor(names(t)[m]), length(trainLabels)))
+}
+
+
+ZeroR <- function(X, targetId) {
+  # ZeroR Algorithm: Finds the most commonly occuring class
+  # 
+  # Args:
+  #  X: data frame or Matrix
+  #  targetId: response/outcome/target/class feature column number
+  
+  # Returns:
+  #  A vector containing the commonly occuring class value and its count  
+  if ( is.character(X[, targetId]) | is.factor(X[, targetId]) ) {
+    u.x <- unique(X[, targetId])
+    u.x.temp <- c()
+    for (i in u.x) {
+      u.x.temp <- c(u.x.temp, sum(X[, targetId] == i))
+    }
+    names(u.x.temp) <- u.x
+    return( c(max(u.x.temp), names(u.x.temp)[which.max(u.x.temp)]) ) 
+  }
+  return(NULL)
+}
+
+OneR <- function(X, targetId) {
+  error.perc <- c()
+  cols <- ncol(X) - 1
+  for (f in 1:cols) {
+    grps <- unique(X[, f])
+    err.count <- c()
+    for (grp in grps) {
+      grp.data <- X[X[, f] == grp, c(f, targetId)]
+      res <- ZeroR(grp.data, 2)
+      err.grp <- sum(as.integer(grp.data[, 2] != res[2]))
+      err.count <- c(err.count, err.grp)
+    }
+    error.perc <- c(error.perc, sum(err.count)/nrow(X))
+  }
+  cat(error.perc)
+  cat("\n")
+  low.err.idx <- which.min(error.perc)
+  return(low.err.idx)
+}
+
+
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Density Paper 2019 Experiments in R
+This repository hosts the experiments we performed using our extended dataset [1] for our paper on Source Code Density.
+
+It holds the raw __`R`__-notebooks and the figures we produced.
+
+# References
+__[1]__ S. Hönel, "359,569 commits with source code density; 1,149 commits of which have software maintenance activity labels (adaptive,corrective,perfective) [data  set]," March 2019. [Online]. Available: https://doi.org/10.5281/zenodo.2590519
